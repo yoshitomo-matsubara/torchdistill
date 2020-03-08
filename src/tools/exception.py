301864b910ace7bc10c4f920de212b67f7f5b24f
Yoshitomo Matsubara <yoshitom@uci.edu>
2020-03-08 11:35:34 -0700

    fixed a condition

--- src/tools/exception.py
+++ src/tools/exception.py
@@ -1,6 +1,6 @@
 class ForwardTerminationException(Exception):
     def __init__(self, *args):
-        self.message = args[0] if args is not None else None
+        self.message = args[0] if args is not None and len(args) > 0 else None
 
     def __str__(self):
         return 'ForwardTerminationException{}'.format(': {}'.format(self.message) if self.message is not None
