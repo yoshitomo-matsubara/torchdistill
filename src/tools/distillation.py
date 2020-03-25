6b66028536ccb2433fbcfac75be58bdcfb1f3ae3
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2020-03-25 11:27:01 -0700

    simplified

--- src/tools/distillation.py
+++ src/tools/distillation.py
@@ -3,20 +3,17 @@ from torch.nn import DataParallel
 from torch.nn.parallel.distributed import DistributedDataParallel
 
 from myutils.pytorch import module_util
-from tools.exception import ForwardTerminationException
 from tools.loss import KDLoss, get_single_loss, get_custom_loss
 
 
-def set_distillation_box_info(module, **kwargs):
-    module.__dict__['distillation_box'] = kwargs
+def set_distillation_box_info(info_dict, module_path, **kwargs):
+    info_dict[module_path] = kwargs
 
 
-def get_distillation_box_info(module):
-    return module.__dict__['distillation_box']
-
-
-def extract_output_hook(self, input, output):
-    self.__dict__['distillation_box']['output'] = output
+def register_forward_hook_with_dict(module, module_path, info_dict):
+    def forward_hook(self, input, output):
+        info_dict[module_path]['output'] = output
+    return module.register_forward_hook(forward_hook)
 
 
 class DistillationBox(nn.Module):
@@ -25,8 +22,10 @@ class DistillationBox(nn.Module):
         self.target_module_handles.clear()
         sub_terms_config = criterion_config.get('sub_terms', None)
         if sub_terms_config is not None:
-            teacher_model = self.teacher_model
-            student_model = self.student_model
+            # TODO: Build teacher and student models with DP/DDP here; Teacher doesn't require grad
+            teacher_model = self.org_teacher_model
+            student_model = self.org_student_model
+
             teacher_model_without_dp =\
                 teacher_model.module if isinstance(teacher_model, DataParallel) else teacher_model
             student_model_without_ddp = \
@@ -36,12 +35,12 @@ class DistillationBox(nn.Module):
                 self.target_module_pairs.append((teacher_path, student_path))
                 teacher_module = module_util.get_module(teacher_model_without_dp, teacher_path)
                 student_module = module_util.get_module(student_model_without_ddp, student_path)
-                set_distillation_box_info(teacher_module, loss_name=loss_name, path_from_root=teacher_path,
-                                          is_teacher=True)
-                set_distillation_box_info(student_module, loss_name=loss_name, path_from_root=student_path,
-                                          is_teacher=False)
-                teacher_handle = teacher_module.register_forward_hook(extract_output_hook)
-                student_handle = student_module.register_forward_hook(extract_output_hook)
+                set_distillation_box_info(self.teacher_info_dict, teacher_path, loss_name=loss_name,
+                                          path_from_root=teacher_path, is_teacher=True)
+                set_distillation_box_info(self.student_info_dict, student_path, loss_name=loss_name,
+                                          path_from_root=student_path, is_teacher=False)
+                teacher_handle = register_forward_hook_with_dict(teacher_module, teacher_path, self.teacher_info_dict)
+                student_handle = register_forward_hook_with_dict(student_module, student_path, self.student_info_dict)
                 self.target_module_handles.append((teacher_handle, student_handle))
 
         org_term_config = criterion_config.get('org_term', dict())
@@ -54,9 +53,10 @@ class DistillationBox(nn.Module):
 
     def __init__(self, teacher_model, student_model, criterion_config):
         super().__init__()
-        self.teacher_model = teacher_model
-        self.student_model = student_model
+        self.org_teacher_model = teacher_model
+        self.org_student_model = student_model
         self.target_module_pairs, self.target_module_handles = list(), list()
+        self.teacher_info_dict, self.student_info_dict = dict(), dict()
         self.org_criterion, self.criterion, self.use_teacher_output = None, None, None
         self.setup(criterion_config)
 
@@ -68,7 +68,7 @@ class DistillationBox(nn.Module):
         student_outputs = self.student_model(sample_batch)
         org_loss_dict = dict()
         if self.check_if_org_loss_required():
-            # Model with auxiliary classifier returns multiple outputs
+            # Models with auxiliary classifier returns multiple outputs
             if isinstance(student_outputs, (list, tuple)):
                 if self.use_teacher_output:
                     for i, sub_student_outputs, sub_teacher_outputs in enumerate(zip(student_outputs, teacher_outputs)):
@@ -82,15 +82,13 @@ class DistillationBox(nn.Module):
                 org_loss_dict = {0: org_loss}
 
         output_dict = dict()
-        teacher_model_without_dp = \
-            self.teacher_model.module if isinstance(self.teacher_model, DataParallel) else self.teacher_model
-        student_model_without_ddp = \
-            self.student_model.module if isinstance(self.student_model, DistributedDataParallel) else self.student_model
         for teacher_path, student_path in self.target_module_pairs:
-            teacher_dict = get_distillation_box_info(module_util.get_module(teacher_model_without_dp, teacher_path))
-            student_dict = get_distillation_box_info(module_util.get_module(student_model_without_ddp, student_path))
-            output_dict[teacher_dict['loss_name']] = ((teacher_dict['path_from_root'], teacher_dict.pop('output')),
-                                                      (student_dict['path_from_root'], student_dict.pop('output')))
+            teacher_module_dict = self.teacher_info_dict[teacher_path]
+            student_module_dict = self.student_info_dict[student_path]
+            output_dict[teacher_module_dict['loss_name']] = (
+                (teacher_module_dict['path_from_root'], teacher_module_dict.pop('output')),
+                (student_module_dict['path_from_root'], student_module_dict.pop('output'))
+            )
 
         total_loss = self.criterion(output_dict, org_loss_dict)
         return total_loss
@@ -113,24 +111,12 @@ class MultiStagesDistillationBox(DistillationBox):
         self.stage_end_epoch = stage1_config['end_epoch']
         print('Stage {}'.format(self.stage_number))
 
-    def sub_forward(self, sample_batch):
-        try:
-            teacher_outputs = self.teacher_model(sample_batch)
-        except ForwardTerminationException as te:
-            teacher_outputs = te
-
-        try:
-            student_outputs = self.student_model(sample_batch)
-        except ForwardTerminationException as se:
-            student_outputs = se
-        return teacher_outputs, student_outputs
-
     def forward(self, sample_batch, targets):
-        teacher_outputs, student_outputs = self.sub_forward(sample_batch)
+        teacher_outputs = self.teacher_model(sample_batch)
+        student_outputs = self.student_model(sample_batch)
         org_loss_dict = dict()
-        if not isinstance(teacher_outputs, ForwardTerminationException) and \
-                not isinstance(student_outputs, ForwardTerminationException) and self.check_if_org_loss_required():
-            # Model with auxiliary classifier returns multiple outputs
+        if self.check_if_org_loss_required():
+            # Models with auxiliary classifier returns multiple outputs
             if isinstance(student_outputs, (list, tuple)):
                 if self.use_teacher_output:
                     for i, sub_student_outputs, sub_teacher_outputs in enumerate(zip(student_outputs, teacher_outputs)):
@@ -144,15 +130,13 @@ class MultiStagesDistillationBox(DistillationBox):
                 org_loss_dict = {0: org_loss}
 
         output_dict = dict()
-        teacher_model_without_dp = \
-            self.teacher_model.module if isinstance(self.teacher_model, DataParallel) else self.teacher_model
-        student_model_without_ddp = \
-            self.student_model.module if isinstance(self.student_model, DistributedDataParallel) else self.student_model
         for teacher_path, student_path in self.target_module_pairs:
-            teacher_dict = get_distillation_box_info(module_util.get_module(teacher_model_without_dp, teacher_path))
-            student_dict = get_distillation_box_info(module_util.get_module(student_model_without_ddp, student_path))
-            output_dict[teacher_dict['loss_name']] = ((teacher_dict['path_from_root'], teacher_dict.pop('output')),
-                                                      (student_dict['path_from_root'], student_dict.pop('output')))
+            teacher_module_dict = self.teacher_info_dict[teacher_path]
+            student_module_dict = self.student_info_dict[student_path]
+            output_dict[teacher_module_dict['loss_name']] = (
+                (teacher_module_dict['path_from_root'], teacher_module_dict.pop('output')),
+                (student_module_dict['path_from_root'], student_module_dict.pop('output'))
+            )
 
         total_loss = self.criterion(output_dict, org_loss_dict)
         return total_loss
