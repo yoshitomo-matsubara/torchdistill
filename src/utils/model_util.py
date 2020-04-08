60b39ecfacb6849f76d2fc484bbe24c23ac0c2e5
Yoshitomo Matsubara <yoshitom@uci.edu>
2020-04-07 23:29:02 -0700

    added adaptation module

--- src/utils/model_util.py
+++ src/utils/model_util.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 from torch.nn import Sequential, DataParallel
 from torch.nn.parallel import DistributedDataParallel
 
+from models.adaptation import get_adaptation_module
 from myutils.pytorch.module_util import get_module, freeze_module_params
 
 
@@ -15,13 +16,16 @@ def redesign_model(org_model, model_config, model_label, device_ids=None):
     print('Redesigning {} model ...'.format(model_label))
     module_dict = OrderedDict()
     frozen_module_path_set = set(model_config.get('frozen_modules', list()))
+    adaptation_dict = model_config.get('adaptations', dict())
     for module_path in module_paths:
-        # TODO: load adaptation modules defined as lists of list (name, module) if module_path matches;
-        #  Names of adaptation modules should not conflict with those of original modules
-        module = get_module(org_model, module_path)
+        if module_path.startswith('+'):
+            module_path = module_path[1:]
+            adaptation_config = adaptation_dict[module_path]
+            module = get_adaptation_module(adaptation_config['type'], **adaptation_config['params'])
+        else:
+            module = get_module(org_model, module_path)
         if module_path in frozen_module_path_set:
             freeze_module_params(module)
-
         module_dict[module_path.replace('.', '__attr__')] = module
 
     model = Sequential(module_dict)
