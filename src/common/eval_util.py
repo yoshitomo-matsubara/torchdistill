11e01b0e69d21549be9d244ad12356511f53f667
Yoshitomo Matsubara <yoshitom@uci.edu>
2020-05-18 15:43:16 -0700

    refactored

--- src/common/eval_util.py
+++ src/common/eval_util.py
@@ -1,16 +1,16 @@
 import torch
 
 
-def compute_accuracy(output, target, topk=(1,)):
+def compute_accuracy(outputs, targets, topk=(1,)):
     """Computes the accuracy over the k top predictions for the specified values of k"""
     with torch.no_grad():
         maxk = max(topk)
-        batch_size = target.size(0)
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target[None])
-        res = []
+        batch_size = targets.size(0)
+        _, preds = outputs.topk(maxk, 1, True, True)
+        preds = preds.t()
+        corrects = preds.eq(targets[None])
+        ressult_list = []
         for k in topk:
-            correct_k = correct[:k].flatten().sum(dtype=torch.float32)
-            res.append(correct_k * (100.0 / batch_size))
-        return res
+            correct_k = corrects[:k].flatten().sum(dtype=torch.float32)
+            ressult_list.append(correct_k * (100.0 / batch_size))
+        return ressult_list
