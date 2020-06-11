463aa9e2dc95ad8df72ed898261b1e23020af8ff
Yoshitomo Matsubara <yoshitom@uci.edu>
2020-06-10 17:01:02 -0700

    simplified

--- image_classification.py
+++ image_classification.py
@@ -9,9 +9,8 @@ from torch.backends import cudnn
 from torch.nn import DataParallel
 from torch.nn.parallel import DistributedDataParallel
 
-from kdkit.common import main_util
 from kdkit.common.constant import def_logger
-from kdkit.common.main_util import load_ckpt, save_ckpt
+from kdkit.common.main_util import is_main_process, init_distributed_mode, load_ckpt, save_ckpt
 from kdkit.datasets import util
 from kdkit.eval.classification import compute_accuracy
 from kdkit.misc.log import setup_log_file, SmoothedValue, MetricLogger
@@ -123,7 +122,7 @@ def distill(teacher_model, student_model, dataset_dict, device, device_ids, dist
         distill_one_epoch(distillation_box, device, epoch, log_freq)
         val_top1_accuracy = evaluate(student_model, distillation_box.val_data_loader, device, device_ids, distributed,
                                      log_freq=log_freq, header='Validation:')
-        if val_top1_accuracy > best_val_top1_accuracy and main_util.is_main_process():
+        if val_top1_accuracy > best_val_top1_accuracy and is_main_process():
             logger.info('Updating ckpt (Best top1 accuracy: '
                         '{:.4f} -> {:.4f})'.format(best_val_top1_accuracy, val_top1_accuracy))
             best_val_top1_accuracy = val_top1_accuracy
@@ -142,10 +141,10 @@ def distill(teacher_model, student_model, dataset_dict, device, device_ids, dist
 
 def main(args):
     log_file_path = args.log
-    if main_util.is_main_process() and log_file_path is not None:
+    if is_main_process() and log_file_path is not None:
         setup_log_file(os.path.expanduser(log_file_path))
 
-    distributed, device_ids = main_util.init_distributed_mode(args.world_size, args.dist_url)
+    distributed, device_ids = init_distributed_mode(args.world_size, args.dist_url)
     logger.info(args)
     cudnn.benchmark = True
     config = yaml_util.load_yaml_file(os.path.expanduser(args.config))
