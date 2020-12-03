bee840df40384594ceb3676e502c46cf6327ccdf
Yoshitomo Matsubara <yoshitom@uci.edu>
2020-12-03 10:42:07 -0800

    added a test case

--- tests/core_test.py
+++ tests/core_test.py
@@ -34,3 +34,18 @@ class ForwardHookManagerUnitTest(TestCase):
         hooked_y = io_dict[target_module_path]['output']
         assert torch.equal(y, hooked_y)
         assert len(fhm.io_dict[target_module_path]) == 0
+
+    def test_pop_io_dict_from_device(self):
+        device = torch.device('cpu')
+        fhm = ForwardHookManager(device)
+        model = models.resnet18(False)
+        target_module_path = 'fc'
+        fhm.add_hook(model, target_module_path, requires_input=False, requires_output=True)
+        x = torch.rand(1, 3, 224, 224)
+        y = model(x)
+        io_dict = fhm.pop_io_dict_from_device(device)
+        assert len(io_dict) == 1
+        assert 'output' in io_dict[target_module_path]
+        hooked_y = io_dict[target_module_path]['output']
+        assert torch.equal(y, hooked_y)
+        assert len(fhm.io_dict[target_module_path]) == 0
