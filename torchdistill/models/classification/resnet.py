583181112b6f69b2235f9db570ab517affe12991
Yoshitomo Matsubara <yoshitom@uci.edu>
2020-12-27 09:38:53 -0800

    cleaned

--- torchdistill/models/classification/resnet.py
+++ torchdistill/models/classification/resnet.py
@@ -105,15 +105,12 @@ class ResNet4Cifar(nn.Module):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
-
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
-
         return x
 
     def forward(self, x: Tensor) -> Tensor:
--- torchdistill/models/classification/wide_resnet.py
+++ torchdistill/models/classification/wide_resnet.py
@@ -84,17 +84,14 @@ class WideResNet(nn.Module):
     def _forward_impl(self, x: Tensor) -> Tensor:
         # See note [TorchScript super()]
         x = self.conv1(x)
-
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.bn1(x)
         x = self.relu(x)
-
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
-
         return x
 
     def forward(self, x: Tensor) -> Tensor:
