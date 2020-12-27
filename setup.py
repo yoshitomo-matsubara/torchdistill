6ccce19375e06476d37ca763fd2f4046eaa108be
Yoshitomo Matsubara <yoshitom@uci.edu>
2020-12-27 10:05:48 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ description = 'A Modular, Configuration-Driven Framework for Knowledge Distillat
               'Trained models, training logs and configurations are available for ensuring the reproducibiliy.'
 setup(
     name='torchdistill',
-    version='0.0.2',
+    version='0.1.0',
     description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/yoshitomo-matsubara/torchdistill',
-    packages=find_packages(exclude=('tests', 'examples', 'config')),
+    packages=find_packages(exclude=('tests', 'examples', 'demo', 'configs')),
     python_requires='>=3.6',
     install_requires=[
-        'torch>=1.6.0',
-        'torchvision>=0.7',
+        'torch>=1.7.1',
+        'torchvision>=0.8.2',
         'numpy',
         'pyyaml',
         'scipy',
