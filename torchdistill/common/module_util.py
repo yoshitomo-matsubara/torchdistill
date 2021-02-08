813d358ba2b3cf6c4f2df5318ba6bd7938cd7272
Yoshitomo Matsubara <yoshitom@uci.edu>
2021-02-07 21:25:27 -0800

    added one more condition to log errors

--- torchdistill/common/module_util.py
+++ torchdistill/common/module_util.py
@@ -41,14 +41,14 @@ def get_module(root_module, module_path):
             if isinstance(module, (DataParallel, DistributedDataParallel)):
                 module = module.module
                 if not hasattr(module, module_name):
-                    if isinstance(module, Sequential):
+                    if isinstance(module, Sequential) and module_name.isnumeric():
                         module = module[int(module_name)]
                     else:
                         logger.info('`{}` of `{}` could not be reached in `{}`'.format(module_name, module_path,
                                                                                        type(root_module).__name__))
                 else:
                     module = getattr(module, module_name)
-            elif isinstance(module, Sequential):
+            elif isinstance(module, Sequential) and module_name.isnumeric():
                 module = module[int(module_name)]
             else:
                 logger.info('`{}` of `{}` could not be reached in `{}`'.format(module_name, module_path,
