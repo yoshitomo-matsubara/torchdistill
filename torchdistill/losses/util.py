8b9453a57f712763661760cd7ab69181176985a4
Yoshitomo Matsubara <yoshitom@uci.edu>
2021-12-23 00:19:04 -0800

    added an option to use teacher output

--- torchdistill/losses/util.py
+++ torchdistill/losses/util.py
@@ -26,12 +26,17 @@ def extract_simple_org_loss(org_criterion, student_outputs, teacher_outputs, tar
 
 
 @register_func2extract_org_output
-def extract_simple_org_loss_dict(org_criterion, student_outputs, teacher_outputs, targets, uses_teacher_output, **kwargs):
+def extract_simple_org_loss_dict(org_criterion, student_outputs, teacher_outputs, targets,
+                                 uses_teacher_output, **kwargs):
     org_loss_dict = dict()
     if isinstance(student_outputs, dict):
+        is_teacher_output_dict = isinstance(teacher_outputs, dict)
         org_loss_dict = dict()
         for key, outputs in student_outputs.items():
-            org_loss_dict[key] = org_criterion(outputs, targets)
+            if uses_teacher_output and is_teacher_output_dict and key in teacher_outputs:
+                org_loss_dict[key] = org_criterion(outputs, teacher_outputs[key], targets)
+            else:
+                org_loss_dict[key] = org_criterion(outputs, targets)
     return org_loss_dict
 
 
