2a62ddb6d1aaaf09585a99f5e1c819d27cc6b186
Yoshitomo Matsubara <yoshitom@uci.edu>
2022-04-13 23:17:08 -0700

    changed minimum python version requirement

--- setup.py
+++ setup.py
@@ -16,15 +16,15 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/yoshitomo-matsubara/torchdistill',
     packages=find_packages(exclude=('tests', 'examples', 'demo', 'configs')),
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
-        'torch>=1.7.1',
-        'torchvision>=0.8.2',
+        'torch>=1.11.0',
+        'torchvision>=0.12.0',
         'numpy',
         'pyyaml>=5.4.1',
         'scipy',
         'cython',
-        'pycocotools>=2.0.1'
+        'pycocotools>=2.0.2'
     ],
     extras_require={
         'test': ['pytest']
