cf9c399a45360620d5a12b971e51a9a9226ceb69
Yoshitomo Matsubara <yoshitom@uci.edu>
2022-12-07 21:08:17 -0800

    enabled naming dataset modules

--- tests/registry_test.py
+++ tests/registry_test.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+from torchdistill.datasets.registry import register_dataset, DATASET_DICT
 from torchdistill.models.registry import get_model
 
 
@@ -10,3 +11,21 @@ class RegistryTest(TestCase):
         kwargs = {'pretrained': True}
         mobilenet_v3 = get_model(model_name, repo_or_dir, **kwargs)
         assert type(mobilenet_v3).__name__ == 'MobileNetV3'
+
+    def test_register_dataset(self):
+        default_dataset_dict_size = len(DATASET_DICT)
+        @register_dataset()
+        class TestDataset1(object):
+            def __init__(self):
+                self.name = 'test1'
+
+        assert 'TestDataset1' in DATASET_DICT
+        assert len(DATASET_DICT) == default_dataset_dict_size + 1
+        random_name = 'custom_test_dataset_name2'
+        @register_dataset(key=random_name)
+        class TestDataset2(object):
+            def __init__(self):
+                self.name = 'test2'
+
+        assert len(DATASET_DICT) == default_dataset_dict_size + 2
+        assert 'TestDataset1' in DATASET_DICT and random_name in DATASET_DICT and 'TestDataset2' not in DATASET_DICT
--- torchdistill/datasets/registry.py
+++ torchdistill/datasets/registry.py
@@ -4,6 +4,17 @@ DATASET_DICT = dict()
 DATASET_DICT.update(torchvision.datasets.__dict__)
 
 
-def register_dataset(cls_or_func):
-    DATASET_DICT[cls_or_func.__name__] = cls_or_func
-    return cls_or_func
+# def register_dataset(cls_or_func):
+#     DATASET_DICT[cls_or_func.__name__] = cls_or_func
+#     return cls_or_func
+
+def register_dataset(*args, **kwargs):
+    def _register_dataset(cls_or_func):
+        key = kwargs.get('key')
+        if key is None:
+            key = cls_or_func.__name__
+
+        DATASET_DICT[key] = cls_or_func
+        return cls_or_func
+
+    return _register_dataset
