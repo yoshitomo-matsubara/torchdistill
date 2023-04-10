1f7a22e4a0ff1556ae1c65ea297224bd26a8e20d
Yoshitomo Matsubara <yoshitom@uci.edu>
2023-04-09 23:40:06 -0700

    Make some common procedures in training box registrable/replaceable

--- torchdistill/core/training.py
+++ torchdistill/core/training.py
@@ -230,8 +230,8 @@ class MultiStagesTrainingBox(TrainingBox):
         self.stage_end_epoch += next_stage_config['num_epochs']
         logger.info('Advanced to stage {}'.format(self.stage_number))
 
-    def post_epoch_process(self, **kwargs):
-        super().post_epoch_process(**kwargs)
+    def post_epoch_process(self, *args, **kwargs):
+        super().post_epoch_process(*args, **kwargs)
         self.current_epoch += 1
         if self.current_epoch == self.stage_end_epoch and self.current_epoch < self.num_epochs:
             self.advance_to_next_stage()
