f708771fc130120e410af5f3acdabf6d156b807e
Yoshitomo Matsubara <yoshitom@uci.edu>
2023-05-06 10:13:50 -0700

    dst_ckpt should be used when using -test_only

--- examples/torchvision/object_detection.py
+++ examples/torchvision/object_detection.py
@@ -227,10 +227,10 @@ def main(args):
     if not args.test_only:
         train(teacher_model, student_model, dataset_dict, dst_ckpt_file_path,
               device, device_ids, distributed, world_size, config, args)
-        student_model_without_ddp =\
-            student_model.module if module_util.check_if_wrapped(student_model) else student_model
-        load_ckpt(dst_ckpt_file_path, model=student_model_without_ddp, strict=True)
 
+    student_model_without_ddp =\
+        student_model.module if module_util.check_if_wrapped(student_model) else student_model
+    load_ckpt(dst_ckpt_file_path, model=student_model_without_ddp, strict=True)
     test_config = config['test']
     test_data_loader_config = test_config['test_data_loader']
     test_data_loader = build_data_loader(dataset_dict[test_data_loader_config['dataset_id']],
