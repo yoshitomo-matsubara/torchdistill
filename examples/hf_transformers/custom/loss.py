07507d2e8483e613e505b208061da599385a9cbf
Yoshitomo Matsubara <yoshitom@uci.edu>
2023-05-08 21:51:43 -0700

    Update sample code for the next major release

--- examples/hf_transformers/custom/loss.py
+++ examples/hf_transformers/custom/loss.py
@@ -2,7 +2,7 @@ from torchdistill.losses.util import register_func2extract_model_output
 
 
 @register_func2extract_model_output
-def extract_transformers_loss(student_outputs, teacher_outputs, targets, uses_teacher_output, **kwargs):
+def extract_transformers_loss(student_outputs, targets, **kwargs):
     model_loss_dict = dict()
     model_loss_dict['loss'] = student_outputs.loss
     return model_loss_dict
