38cc97ef237033210ccb37abfd2cd729d636db8c
Yoshitomo Matsubara <yoshitom@uci.edu>
2023-08-26 17:48:36 -0700

    Add docstrings

--- torchdistill/core/interfaces/pre_epoch_proc.py
+++ torchdistill/core/interfaces/pre_epoch_proc.py
@@ -7,6 +7,14 @@ logger = def_logger.getChild(__name__)
 
 @register_pre_epoch_proc_func
 def default_pre_epoch_process_with_teacher(self, epoch=None, **kwargs):
+    """
+    Performs pre-epoch process for distillation box.
+
+    :param self: distillation box.
+    :type self: torchdistill.core.distillation.DistillationBox or torchdistill.core.training.TrainingBox
+    :param epoch: `epoch` for DistributedSampler.
+    :type epoch: int
+    """
     clear_io_dict(self.teacher_io_dict)
     clear_io_dict(self.student_io_dict)
     self.student_model.train()
@@ -16,6 +24,14 @@ def default_pre_epoch_process_with_teacher(self, epoch=None, **kwargs):
 
 @register_pre_epoch_proc_func
 def default_pre_epoch_process_without_teacher(self, epoch=None, **kwargs):
+    """
+    Performs pre-epoch process for training box.
+
+    :param self: distillation box.
+    :type self: torchdistill.core.distillation.DistillationBox or torchdistill.core.training.TrainingBox
+    :param epoch: `epoch` for DistributedSampler.
+    :type epoch: int
+    """
     clear_io_dict(self.model_io_dict)
     self.model.train()
     if self.distributed:
