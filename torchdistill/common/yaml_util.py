f94a41e96f7f594bbda61ec8c0efb5170f56b54e
Yoshitomo Matsubara <yoshitom@uci.edu>
2024-03-22 19:51:49 -0700

    Rename functions

--- torchdistill/common/yaml_util.py
+++ torchdistill/common/yaml_util.py
@@ -114,7 +114,7 @@ def yaml_setattr(loader, node):
     return args[0]
 
 
-def yaml_simple_access(loader, node):
+def yaml_access_by_index_or_key(loader, node):
     """
     Obtains a value from a specified data
 
@@ -150,6 +150,6 @@ def load_yaml_file(yaml_file_path, custom_mode=True):
         yaml.add_constructor('!import_call_method', yaml_import_call_method, Loader=yaml.FullLoader)
         yaml.add_constructor('!getattr', yaml_getattr, Loader=yaml.FullLoader)
         yaml.add_constructor('!setattr', yaml_getattr, Loader=yaml.FullLoader)
-        yaml.add_constructor('!simple_access', yaml_simple_access, Loader=yaml.FullLoader)
+        yaml.add_constructor('!access_by_index_or_key', yaml_access_by_index_or_key, Loader=yaml.FullLoader)
     with open(yaml_file_path, 'r') as fp:
         return yaml.load(fp, Loader=yaml.FullLoader)
