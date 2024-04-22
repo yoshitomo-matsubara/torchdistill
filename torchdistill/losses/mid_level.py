81cc2246e8b04b6b929a2d67d50b67343e23b787
Yoshitomo Matsubara <yoshitom@uci.edu>
2024-04-21 17:31:46 -0700

    Add KD with logits standardization

--- torchdistill/losses/mid_level.py
+++ torchdistill/losses/mid_level.py
@@ -1677,3 +1677,58 @@ class SRDLoss(nn.Module):
                                  torch.softmax(teacher_logits / self.temperature, dim=1))
         loss = 2 * feat_distill_loss + kl_loss
         return loss
+
+
+@register_mid_level_loss
+class LogitStdKDLoss(nn.KLDivLoss):
+    """
+    A standard knowledge distillation (KD) loss module with logits standardization.
+
+    Shangquan Sun, Wenqi Ren, Jingzhi Li, Rui Wang, Xiaochun Cao: `"Logit Standardization in Knowledge Distillation" <https://arxiv.org/abs/2403.01427>`_ @ CVPR 2024 (2024)
+
+    :param student_module_path: student model's logit module path.
+    :type student_module_path: str
+    :param student_module_io: 'input' or 'output' of the module in the student model.
+    :type student_module_io: str
+    :param teacher_module_path: teacher model's logit module path.
+    :type teacher_module_path: str
+    :param teacher_module_io: 'input' or 'output' of the module in the teacher model.
+    :type teacher_module_io: str
+    :param temperature: hyperparameter :math:`\\tau` to soften class-probability distributions.
+    :type temperature: float
+    :param eps: value added to the denominator for numerical stability.
+    :type eps: float
+    :param alpha: balancing factor for :math:`L_{CE}`, cross-entropy.
+    :type alpha: float
+    :param beta: balancing factor (default: :math:`1 - \\alpha`) for :math:`L_{KL}`, KL divergence between class-probability distributions softened by :math:`\\tau`.
+    :type beta: float or None
+    :param reduction: ``reduction`` for KLDivLoss. If ``reduction`` = 'batchmean', CrossEntropyLoss's ``reduction`` will be 'mean'.
+    :type reduction: str or None
+    """
+    def __init__(self, student_module_path, student_module_io, teacher_module_path, teacher_module_io,
+                 temperature, eps=1e-7, alpha=None, beta=None, reduction='batchmean', **kwargs):
+        super().__init__(reduction=reduction)
+        self.student_module_path = student_module_path
+        self.student_module_io = student_module_io
+        self.teacher_module_path = teacher_module_path
+        self.teacher_module_io = teacher_module_io
+        self.temperature = temperature
+        self.eps = eps
+        self.alpha = alpha
+        self.beta = 1 - alpha if beta is None else beta
+        cel_reduction = 'mean' if reduction == 'batchmean' else reduction
+        self.cross_entropy_loss = nn.CrossEntropyLoss(reduction=cel_reduction, **kwargs)
+
+    def standardize(self, logits):
+        return (logits - logits.mean(dim=-1, keepdims=True)) / (self.eps + logits.std(dim=-1, keepdims=True))
+
+    def forward(self, student_io_dict, teacher_io_dict, targets=None, *args, **kwargs):
+        student_logits = student_io_dict[self.student_module_path][self.student_module_io]
+        teacher_logits = teacher_io_dict[self.teacher_module_path][self.teacher_module_io]
+        soft_loss = super().forward(torch.log_softmax(self.standardize(student_logits) / self.temperature, dim=1),
+                                    torch.softmax(self.standardize(teacher_logits) / self.temperature, dim=1))
+        if self.alpha is None or self.alpha == 0 or targets is None:
+            return soft_loss
+
+        hard_loss = self.cross_entropy_loss(student_logits, targets)
+        return self.alpha * hard_loss + self.beta * (self.temperature ** 2) * soft_loss
