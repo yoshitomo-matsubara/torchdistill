85d7f66354ea7586780acfd4e35632e5187a9a55
Yoshitomo Matsubara <dr.yoshitomo.matsubara@gmail.com>
2024-12-08 00:09:13 -0800

    Migrate all bottleneck-injected models to sc2bench

--- torchdistill/models/__init__.py
+++ torchdistill/models/__init__.py
@@ -1,9 +1,7 @@
 from . import adaptation, classification
-from .custom import CUSTOM_MODEL_DICT
 from .registry import ADAPTATION_MODULE_DICT, AUXILIARY_MODEL_WRAPPER_DICT
 
 MODEL_DICT = dict()
 
 MODEL_DICT.update(ADAPTATION_MODULE_DICT)
 MODEL_DICT.update(AUXILIARY_MODEL_WRAPPER_DICT)
-MODEL_DICT.update(CUSTOM_MODEL_DICT)
--- torchdistill/models/custom/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import bottleneck
-from ..registry import MODEL_DICT
-
-CUSTOM_MODEL_DICT = dict()
-
-CUSTOM_MODEL_DICT.update(MODEL_DICT)
