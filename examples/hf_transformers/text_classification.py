2948bf680f5425abd657d1d8d87b967ea43bfb15
Yoshitomo Matsubara <dr.yoshitomo.matsubara@gmail.com>
2025-05-10 22:05:56 -0700

    Support multiple metrics

--- examples/hf_transformers/text_classification.py
+++ examples/hf_transformers/text_classification.py
@@ -23,14 +23,14 @@ import argparse
 import logging
 import os
 import time
+import numpy as np
 
 import datasets
 import torch
 import transformers
 from accelerate import Accelerator, DistributedType
 from torch.backends import cudnn
-from transformers import AutoConfig, AutoModelForSequenceClassification, AutoTokenizer, DataCollatorWithPadding, \
-    PretrainedConfig
+from transformers import AutoConfig, AutoModelForSequenceClassification, AutoTokenizer, DataCollatorWithPadding
 
 from custom.dataset import preprocess_hf_text_datasets
 from custom.optim import customize_lr_config
@@ -94,49 +94,62 @@ def train_one_epoch(training_box, epoch, log_freq):
 
 
 @torch.inference_mode()
-def evaluate(model, data_loader, metric, evaluate_config, accelerator, title=None, header='Test: '):
+def evaluate(model, data_loader, metric_dict, evaluate_config, accelerator, title=None, header='Test: '):
     if title is not None:
         logger.info(title)
 
-    uses_argmax = evaluate_config.get('argmax', True)
-    thresholds = evaluate_config.get('thresholds', None)
-    if isinstance(thresholds, (list, tuple)):
-        thresholds = torch.Tensor(thresholds)
-
-    add_batch_kwargs = evaluate_config.get('add_batch', dict())
     model.eval()
+    evaluate_metrics_config = evaluate_config.get('metrics', dict())
     for batch in data_loader:
         labels = batch.pop('labels')
         outputs = model(**batch)
-        if uses_argmax:
-            predictions = outputs.logits.argmax(dim=-1)
-            metric.add_batch(
-                predictions=accelerator.gather(predictions),
-                references=accelerator.gather(labels),
-                **add_batch_kwargs
-            )
-        elif thresholds is not None:
-            predictions = (outputs.logits > thresholds).int()
-            metric.add_batch(
-                predictions=accelerator.gather(predictions),
-                references=accelerator.gather(labels),
-                **add_batch_kwargs
-            )
-        else:
-            metric.add_batch(
-                prediction_scores=accelerator.gather(outputs.logits),
-                references=accelerator.gather(labels),
-                **add_batch_kwargs
-            )
+        for metric_key, metric in metric_dict.items():
+            sub_evaluate_metrics_config = evaluate_metrics_config.get(metric_key, dict())
+            uses_argmax = sub_evaluate_metrics_config.get('argmax', True)
+            thresholds = sub_evaluate_metrics_config.get('thresholds', None)
+            if isinstance(thresholds, (list, tuple)):
+                thresholds = torch.Tensor(thresholds)
 
-    compute_kwargs = evaluate_config.get('compute', dict())
-    eval_dict = metric.compute(**compute_kwargs)
-    eval_desc = header + ', '.join([f'{key} = {eval_dict[key]}' for key in sorted(eval_dict.keys())])
-    logger.info(eval_desc)
-    return eval_dict
+            add_batch_kwargs = sub_evaluate_metrics_config.get('add_batch', dict())
+            if uses_argmax:
+                predictions = outputs.logits.argmax(dim=-1)
+                metric.add_batch(
+                    predictions=accelerator.gather(predictions),
+                    references=accelerator.gather(labels),
+                    **add_batch_kwargs
+                )
+            elif thresholds is not None:
+                predictions = (outputs.logits > thresholds).int()
+                metric.add_batch(
+                    predictions=accelerator.gather(predictions),
+                    references=accelerator.gather(labels),
+                    **add_batch_kwargs
+                )
+            else:
+                metric.add_batch(
+                    prediction_scores=accelerator.gather(outputs.logits),
+                    references=accelerator.gather(labels),
+                    **add_batch_kwargs
+                )
+
+    eval_dict = dict()
+    for metric_key, metric in metric_dict.items():
+        sub_evaluate_metrics_config = evaluate_metrics_config.get(metric_key, dict())
+        compute_kwargs = sub_evaluate_metrics_config.get('compute', dict())
+        sub_eval_dict = metric.compute(**compute_kwargs)
+        eval_desc = header + ', '.join([f'{key} = {sub_eval_dict[key]}' for key in sorted(sub_eval_dict.keys())])
+        logger.info(eval_desc)
+        for key, value in list(sub_eval_dict.items()):
+            if isinstance(value, (list, tuple, np.ndarray)):
+                mean_value = sum(value) / len(value)
+                sub_eval_dict[key] = mean_value
+                logger.info(f'Replacing the list of {key} values with the averaged value = {mean_value}')
+        eval_dict.update(sub_eval_dict)
+    main_value = eval_dict[evaluate_config['main_metric']]
+    return main_value, eval_dict
 
 
-def train(teacher_model, student_model, dataset_dict, dst_ckpt_dir_path, metric, evaluate_config,
+def train(teacher_model, student_model, dataset_dict, dst_ckpt_dir_path, metric_dict, evaluate_config,
           device, device_ids, distributed, config, args, accelerator):
     logger.info('Start training')
     train_config = config['train']
@@ -151,8 +164,9 @@ def train(teacher_model, student_model, dataset_dict, dst_ckpt_dir_path, metric,
     for epoch in range(training_box.num_epochs):
         training_box.pre_epoch_process(epoch=epoch)
         train_one_epoch(training_box, epoch, log_freq)
-        val_dict = evaluate(student_model, training_box.val_data_loader, metric, evaluate_config, accelerator, header='Validation: ')
-        val_value = sum(val_dict.values())
+        val_value, val_dict = evaluate(
+            student_model, training_box.val_data_loader, metric_dict, evaluate_config, accelerator, header='Validation: '
+        )
         if val_value > best_val_number:
             logger.info('Updating ckpt at {}'.format(dst_ckpt_dir_path))
             best_val_number = val_value
@@ -224,10 +238,10 @@ def main(args):
     )
 
     # Get the metric function
-    metric = config['metric']
+    metric_dict = config['metrics']
     evaluate_config = config['evaluate']
     if not args.test_only:
-        train(teacher_model, student_model, dataset_dict, dst_ckpt_dir_path, metric, evaluate_config,
+        train(teacher_model, student_model, dataset_dict, dst_ckpt_dir_path, metric_dict, evaluate_config,
               device, device_ids, distributed, config, args, accelerator)
         student_tokenizer.save_pretrained(dst_ckpt_dir_path)
 
@@ -240,13 +254,13 @@ def main(args):
     cudnn.deterministic = True
     if not args.student_only and teacher_model is not None:
         teacher_model = teacher_model.to(accelerator.device)
-        evaluate(teacher_model, test_data_loader, metric, evaluate_config, accelerator,
+        evaluate(teacher_model, test_data_loader, metric_dict, evaluate_config, accelerator,
                  title='[Teacher: {}]'.format(teacher_model_config['key']))
 
     # Reload the best checkpoint based on validation result
     student_tokenizer, student_model = load_tokenizer_and_model(student_model_config, task_name, True)
     student_model = accelerator.prepare(student_model)
-    evaluate(student_model, test_data_loader, metric, evaluate_config, accelerator,
+    evaluate(student_model, test_data_loader, metric_dict, evaluate_config, accelerator,
              title='[Student: {}]'.format(student_model_config['key']))
 
 
